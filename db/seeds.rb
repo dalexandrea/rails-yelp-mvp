a4a7d63f1ed53720a3c212e8938c7833733304cb
Andrea dAlessandro <dalexandrea@gmail.com>
2017-11-29 11:48:00 +0100

    Added Seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Started seed"
+puts "Emptying DB!"
+
+Restaurant.destroy_all
+
+categories = %w(chinese italian japanese french belgian)
+
+10.times do
+  resto = Restaurant.new(name: Faker::Food.ingredient, phone: Faker::PhoneNumber.phone_number, address: Faker::Address.street_address, category: categories.sample)
+
+  puts "#{resto.name} saved!" if resto.save
+end
+
+puts "Seeding finished!"
